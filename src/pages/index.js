4a51ecd6ec0015867eb460e12a8a3ebd14ba9368
Olvine23 <olivinegeorge@gmail.com>
2021-08-21 01:46:11 -0700

    added navigation

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,23 +4,74 @@ import { StaticImage  } from "gatsby-plugin-image";
 import logo from "../images/OK.jpg"
 const Home = () => {
   return (
-    <div class = " container mx-aut bg-gray-200">
+    <div class = " container mx-auto bg-gray-200">
+      <nav class="flex items-center justify-between flex-wrap bg-gradient-to-b from-yellow-400 to-red-500 p-9 md:decoration-slice p-6">
+  <div class="flex items-center flex-shrink-0 text-white mr-6">
+    <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+    <span class="font-semibold text-xl tracking-tight">OlvineWind</span>
+  </div>
+  <div class="block lg:hidden">
+    <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+      <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+    </button>
+  </div>
+  <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+    <div class="text-sm lg:flex-grow">
+      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+        Docs
+      </a>
+      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+        Examples
+      </a>
+      <a href="https://olvinebloggermain.gatsbyjs.io/" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+        Blog
+      </a>
+    </div>
+    <div>
+      <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
+    </div>
+  </div>
+</nav>
     <p class = "decoration-clone bg-gradient-to-b from-yellow-400 to-red-500 p-9 md:decoration-slice ...">popo</p>
     <img  src = {logo} class = " h-60 w-80 float-right p-9 ..."></img>
-    <h1 class = "p-9 text-5xl font-bold font-body">Hello Olvine</h1>
+    <h1 class = "p-9 text-5xl font-bold font-body ">Hello Olvine</h1>
     <p class = "p-5 font-body text-xl tracking-tight md:tracking-wide">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit.  <br></br>Maecenas efficitur ligula aliquam nibh sagittis, ac vehicula nunc dapibus. Quisque faucibus sapien augue, a hendrerit erat dictum in. Suspendisse vestibulum erat ut magna bibendum consequat. Nulla lorem massa, mollis ut felis sed, auctor vestibulum eros. Nam semper a felis sed eleifend. Nullam orci libero, varius in ullamcorper ac, feugiat ac massa. Suspendisse consequat et dui nec condimentum. Quisque ut lacus augue. Nunc eget gravida diam. Quisque dapibus ex vitae rutrum gravida. Nulla lobortis lacinia felis, ultricies vehicula elit mattis sit amet. Phasellus id ipsum eget ex tincidunt convallis. Praesent libero mauris, interdum eget dignissim ut, pharetra a tellus. Aenean nec eleifend libero. Mauris a blandit urna. Maecenas consectetur ultricies felis non gravida.
 
 Sed quis ex eu nisi semper congue at id ex. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis facilisis efficitur lorem ut dignissim. Donec bibendum erat sit amet lectus rutrum, sit amet ultrices orci blandit. Nam ultrices semper neque sit amet ultricies. Quisque at sapien ac tellus iaculis rutrum sed id lectus. Phasellus nec erat pharetra, elementum sapien eget, ultricies ante. Praesent dapibus eu lectus sed porta. Praesent quis hendrerit velit.
 
-Etiam mollis nisl nec neque aliquam, in consectetur sem malesuada. Nulla justo quam, feugiat quis imperdiet ac, elementum non augue. Aliquam euismod, neque eu volutpat aliquet, sem orci semper lorem, vitae imperdiet nibh libero consectetur odio. Aenean id neque quam. Mauris fermentum tortor ipsum, ac luctus nibh consequat et. Mauris interdum, odio quis pellentesque molestie, risus lectus feugiat ex, vitae auctor orci tortor in sapien. Cras suscipit aliquam mauris, vitae gravida purus facilisis sed.
+Etiam mollis nisl nec neque aliquam, in consectetur sem malesuada.
+<br></br> 
+ <h2 class = "animate-spin w-5 h-5 mr-3 "  viewBox="0 0 24 24">Loading</h2> <br></br>
 
+    <br></br>
+     Nulla justo quam, feugiat quis imperdiet ac, elementum non augue. Aliquam euismod, neque eu volutpat aliquet, sem orci semper lorem, vitae imperdiet nibh libero consectetur odio. Aenean id neque quam. Mauris fermentum tortor ipsum, ac luctus nibh consequat et. Mauris interdum, odio quis pellentesque molestie, risus lectus feugiat ex, vitae auctor orci tortor in sapien. Cras suscipit aliquam mauris, vitae gravida purus facilisis sed.
+ 
+         
 Donec imperdiet felis ut imperdiet scelerisque. Vivamus tristique magna at risus vulputate, sit amet varius nunc consectetur. Maecenas scelerisque mauris gravida mi bibendum pretium. Nam facilisis, libero scelerisque aliquet accumsan, mauris magna viverra nisl, at lobortis urna nisl eu ante. Sed quis porta quam. Maecenas imperdiet nisl quis orci imperdiet, et sagittis nunc facilisis. Curabitur vitae velit vulputate, tristique lectus quis, finibus sem. Pellentesque varius vestibulum ultricies. Nunc ut ex eros. Etiam eu sollicitudin sapien.
 
 Maecenas nec sem eu odio ullamcorper fringilla. Duis urna mi, placerat vel eros a, suscipit facilisis eros. Vivamus a leo tempus, laoreet orci eu, rutrum ipsum. Integer finibus dolor a nisl faucibus placerat. Sed faucibus commodo velit, et vestibulum ex viverra in. Nam sit amet lacus nec enim bibendum pulvinar. Sed viverra ornare nisl blandit lobortis. Duis sed massa vitae lacus posuere consequat. Proin convallis pharetra mi eu placerat. Suspendisse eu commodo est. Nulla et est dapibus, pharetra enim a, rhoncus urna. Nulla nibh ligula, lacinia vel leo sed, malesuada suscipit neque. Integer ut libero mi.
 
 Generated 5 paragraphs, 442 words, 2983 bytes of Lorem Ipsum
     </p>
+    <button class="transition duration-500 ease-in-out bg-blue-600 hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 ...">
+  Hover me
+</button>
+
+<div class="max-w-sm rounded overflow-hidden shadow-lg">
+  <img class="w-full" src={logo} alt="Sunset in the mountains"></img>
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <p class="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div class="px-6 pt-4 pb-2">
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+  </div>
+</div>
     </div>
      
 
